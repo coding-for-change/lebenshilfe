79be8d4aeffb71bf12593cd85484be36abb6107a
jakoblandbrecht <jakob.landbrecht@tum.de>
2026-04-26 23:25:58 +0200

    Remove unused mail transporter import from auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-import { transporter } from "./mail";
 import { Role, SchulbegleiterStatus } from "@/generated/prisma";
 import { sendMail } from "./mail";
 
